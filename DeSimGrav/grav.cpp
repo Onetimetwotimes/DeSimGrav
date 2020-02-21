3a782197c687bae890072d91848253af33187df0
Simeon Widdis <SWiddis@student.neumont.edu>
2020-02-21 13:36:21 -0700

    Simple Testing Code in backend-testing main

--- DeSimGrav/grav.cpp
+++ DeSimGrav/grav.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include "particle.h"
 
 int main() {
-	std::vector<int> v = std::vector<int>(5);
-	std::cout << v.size() << std::endl;
+	int dim = 2;
+	Particle p = Particle(dim);
+	std::vector<double> v = std::vector<double>(dim);
+	v.at(0) = 1.0;
+	v.at(1) = 2.0;
+	p.setVelocity(v);
+	for (int i = 0; i < dim; i++)
+		std::cout << p.getVelocity().at(i) << std::endl;
 }
